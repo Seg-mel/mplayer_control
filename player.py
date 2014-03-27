c29d21cc5949d74f60abb9a870a25eab7db8dd32
Seg-mel <melomansegfault@gmail.com>
2014-03-28 03:20:29 +0400

    1) Edited type test in _new_args_method in Player class 2) Created _setter method in Properties class 3) Some fixes

--- player.py
+++ player.py
@@ -45,9 +45,47 @@ class Properties(object):
         print 'EXECUTED GET PROPERTY:', item['command'], answer
         return answer.split('=')[-1][:-1]
 
-    def _setter(self, args):
+    def _setter(self, args, item):
         ## Setter for property
-        print args
+
+        def type_test(arg):
+            ## Type test function
+            basic_type = item['type']
+            gotten_type = str(type(arg))
+            if basic_type not in gotten_type:
+                # Permit for setting integer arg in float arg
+                if (basic_type == 'float') and ('int' in gotten_type):
+                    pass
+                # Type test for flag type
+                elif ('int' in gotten_type) and (basic_type == 'flag'):
+                    if (arg != 0) and (arg != 1):
+                        error_string = 'the value must be 0 or 1'
+                        raise ValueError(error_string)
+                # Test for other types
+                else:
+                    error_string = "wrong type of input arg for '%s = <%s>'" %\
+                                    (item['command'], item['type'])
+                    raise TypeError(error_string)
+
+        # Count args test
+        if type(args) is tuple:
+            args_length = len(args)
+            error_string = "property '%s' takes exactly 1 arguments (%d given)"
+            raise TypeError(error_string % (item['command'], args_length))
+        # Type test
+        type_test(args)
+        # Min/max test
+        if (item['min'] is not False) and (args < item['min']):
+            error_string = "the value must be min=%d"
+            raise ValueError(error_string % item['min'])
+        if (item['max'] is not False) and (args > item['max']):
+            error_string = "the value must be max=%d"
+            raise ValueError(error_string % item['max'])
+        # Sending the command
+        command_string = 'pausing_keep set_property %s %s' %\
+                                                   (item['command'], str(args))
+        self._send_command(command_string)
+        print 'EXECUTED SET PROPERTY:', item['command'], args
 
     def __new__(cls, *args, **kwargs):
 
@@ -71,7 +109,7 @@ class Properties(object):
                 max_info = '\nmax:\t%s' % item['max']
             type_info = 'type:\t%s' % item['type']
             doc  = '%s\n%s%s%s%s' % (doc_info, type_info, min_info, 
-                                                    max_info, set_info)
+                                                max_info, set_info)
             return doc
 
         # Create new class properties from mplayer property_dict
@@ -80,14 +118,15 @@ class Properties(object):
             if property_dict[item]['set'] is True:
                 # Create property with set capacity
                 prop = property(fget=partial(cls._getter, 
-                                          item=property_dict[item]), 
-                             fset=cls._setter, 
-                             doc=doc)
+                                             item=property_dict[item]), 
+                                fset=partial(cls._setter, 
+                                             item=property_dict[item]),
+                                doc=doc)
             elif property_dict[item]['set'] is False:
                 # Create property without set capacity
                 prop = property(fget=partial(cls._getter, 
-                                          item=property_dict[item]),
-                             doc=doc)
+                                             item=property_dict[item]),
+                                doc=doc)
             setattr(cls, item, prop)
         return super(Properties, cls).__new__(cls)
 
@@ -145,7 +184,7 @@ class Player(object):
             ## Return error string for args type error
             args_string = ''
             for t in item['types']:
-                args_string += ('%s, ' % t)
+                args_string += ('<%s>, ' % t)
             error_string = 'wrong types of input args in %s(%s)' % \
                             (item['command'],args_string[:-2])
             return error_string
@@ -155,6 +194,19 @@ class Player(object):
             error_string = '%s() takes exactly %d arguments (%d given)' % \
                             (item['command'], count_args_types, count_args)
             return error_string
+
+        def type_test(num, arg):
+            ## Type test function
+            basic_type = item['types'][num]
+            gotten_type = str(type(arg))
+            # print basic_type, gotten_type , arg
+            if basic_type not in gotten_type:
+                # Permit for setting integer arg in float arg
+                if (basic_type == 'float') and ('int' in gotten_type):
+                    pass
+                else:
+                    # self._send_command('quit')
+                    raise TypeError(get_args_error_string())
 
         # For single value, because it is not a tuple
         if type(args) is not tuple:
@@ -167,21 +219,14 @@ class Player(object):
             # Append command string
             for num, value in enumerate(args):
                 # Test on type
-                basic_type = item['types'][num]
-                gotten_type = str(type(value))
-                # print basic_type, gotten_type , value
-                if basic_type not in gotten_type:
-                    # Permit for setting integer value in float value
-                    if (basic_type == 'float') and ('int' in gotten_type):
-                        pass
-                    else:
-                        self._send_command('quit') 
-                        raise TypeError(get_args_error_string())
+                type_test(num, value)
+                # Create command string
                 command_string += ' %s' % str(value)
             print 'EXECUTED VALUES COMMAND:', command_string
+            # Sending the command
             self._send_command(command_string)
         else:
-            self._send_command('quit')
+            # self._send_command('quit')
             raise TypeError(get_count_args_error_string())
 
     def __new__(cls, *args, **kwargs):
@@ -231,11 +276,6 @@ class Player(object):
 
     def __init__(self, mplayer=MPLAYER_PATH, fifo=FIFO_PATH, 
                                                            stdout=STDOUT_PATH):
-        # fifo_template = pipes.Template()
-        # if os.path.exists(FIFO_PATH):
-        #     os.remove(FIFO_PATH)
-        # fifo_open = fifo_template.open(FIFO_PATH, 'w')
-
         # Reassigning class property 'properties'.
         # This operation is need for normal help of this class 
         self.properties = self.properties()
@@ -267,18 +307,21 @@ if __name__=='__main__':
     # player.loadfile("/home/meloman/data/tmp/audiotest/8march.ogg")
     # player.use_master()
     # player.volume(100)
-    a = 1
-    while a==1:
-        time.sleep(1)
-        print '~'*79
-    #     player.get_percent_pos()
-    #     player.get_time_pos()
-    #     player.get_time_length()
-    #     player.get_file_name()
-    #     player.get_meta_title()
-        player.properties.volume
-        player.properties.audio_bitrate
-        player.properties.channels
-        player.properties.length
-        player.properties.percent_pos
-        player.properties.stream_length
+    for i in range(101):
+        time.sleep(0.1)
+        player.properties.volume = i
+    # a = 1
+    # while a==1:
+    #     time.sleep(1)
+    #     print '~'*79
+        # player.get_percent_pos()
+        # player.get_time_pos()
+        # player.get_time_length()
+        # player.get_file_name()
+        # player.get_meta_title()
+        # player.properties.volume
+        # player.properties.audio_bitrate
+        # player.properties.channels
+        # player.properties.length
+        # player.properties.percent_pos
+        # player.properties.stream_length
